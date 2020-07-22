d4b9b4923f97cea6f1633301aee2a1070c0b8d9a
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-22 15:44:14 -0700

    update description on Landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,12 +5,9 @@ const Landing = () => {
     <div style={{ textAlign: "center" }}>
       <h1>Email me</h1>
       <p class="flow-text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, ullam
-        ipsa, molestias facere a nihil repellendus voluptatem debitis ut minus
-        aut provident eius explicabo cumque eaque ad iste nam veritatis.Totam
-        delectus, dicta eos voluptate vel autem, inventore, magni sit quasi odio
-        quis qui. Magni saepe vitae excepturi dicta architecto eaque esse
-        inventore facilis quos ut, suscipit, quam cum odio.
+        Regardless of the audience type, survey email plays a key role in
+        persuading ... feedback is and how it will help you improve your product
+        or service.
       </p>
     </div>
   );
